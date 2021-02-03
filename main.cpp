83d957b959ca5b9f8b6667a5e195a574192763b3
Rafael Arce Nazario <rafael.arce@upr.edu>
2021-02-03 17:59:20 -0400

    Cambios a cmake

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() 
+#define CATCH_CONFIG_MAIN
 #include "catch_amalgamated.hpp"
 #include <vector>
 #include <iostream>
 using namespace std;
 
+// Esta es la versión donde la función puede modificar los
+// vectores.
 bool equalWOZeroes_v01( vector<int> &A, vector<int> &B ) {
     return false;
 }
 
 
-
+// Esta es la versión donde los vectores no se pueden modificar
 bool equalWOZeroes_v02( const vector<int> &A, const vector<int> &B ) {
     return false;
 }
@@ -23,12 +25,18 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
 	vector<int> E {0,0,0,0,0};
 	vector<int> F {0,0,0,0,0};
 
+//    Descomenta estas pruebas cuando implementes la
+//    segunda version
 
 //    CHECK( equalWOZeroes_v02(A,B) );
 //    CHECK( equalWOZeroes_v02(A,C) );
 //    CHECK( equalWOZeroes_v02(A,D) == false );
 //    CHECK( equalWOZeroes_v02(D,D)  );
 //    CHECK( equalWOZeroes_v02(E,F)  );
+
+
+//    Descomenta estas pruebas cuando implementes la
+//    primera version
 
 //    CHECK( equalWOZeroes_v01(A,B) );
 //    CHECK( equalWOZeroes_v01(A,C) );
